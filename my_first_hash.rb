e694d54904bb2d916dbcf23036d2de0764239512
MARUF B AWAL <marufbinawal@gmail.com>
2019-05-11 22:12:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,8 +38,9 @@ def adding
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  shipping_manifest["gun poweder" => 4]
 
   # return the shipping_manifest hash below
-
+  push shipping_manifest
+  
 end
